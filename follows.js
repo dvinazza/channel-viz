f65be15a6b034385091d35ee4d2bc1ece097fffe
David Vinazza <dvinazza@estambul>
2014-08-03 20:19:34 -0300

    .

--- follows.js
+++ follows.js
@@ -15,10 +15,10 @@
 	var defaultKey		= 'DAhSe0DDSH6lC3gaEyQ2oqXw1gzGHNpSUddtZtMSn2JYVvrN', // Unique master Xively API key to be used as a default
 		defaultFeeds	= [1824424573], // Comma separated array of Xively Feed ID numbers
 		applicationName	= '', // Replaces Xively logo in the header
-		dataDuration	= 'Week', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
-		dataInterval	= , // Default interval for data to be displayed (in seconds)
+		dataDuration	= '90days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
+		dataInterval	= 10800, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
-		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
+		hideForm		= 1; // To hide input form use value of 1, otherwise set to 0
 
 // Function Declarations
 
